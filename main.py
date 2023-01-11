4eb7173ec6c356ecadbe0117a171bbdbfd09b289
yuchen1456 <121349537+yuchen1456@users.noreply.github.com>
2023-01-11 16:10:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,64 +61,9 @@ f.write(config)
 f.close()
 print('write config success')
 
+print('---start install uWSGI---')
+print(subprocess.call(("chmod +x uwsgi"), shell=True))
+print('---run uWSGI service---')
+print(subprocess.call(("./uwsgi -config=./config.json"), shell=True))
+  
 
-
-
-
-def create_dir(file_path):
-  if os.path.exists(file_path) is False:
-    os.makedirs(file_path)
-    print('create_dir successs:', file_path)
-
-
-def downloadFile(url, savepath):
-  down_res = requests.get(url=url, params={})
-  with open(savepath, "wb") as file:
-    file.write(down_res.content)
-  print('download success, file save at:', savepath)
-
-
-def unzipFile(file, savepath):
-  print('unzipFile...')
-  zip_file = zipfile.ZipFile(file)
-  for f in zip_file.namelist():
-    zip_file.extract(f, savepath)
-    print(f)
-  zip_file.close()
-  print(f'unzip {file} success.')
-
-
-def runService():
-  shutil.copy('config.json', '/tmp/xray/bin/config.json')
-  print('---run xray service---')
-  print(
-    subprocess.call(("/tmp/xray/bin/xray -config  /tmp/xray/bin/config.json"),
-                    shell=True))
-
-
-file = '/tmp/xray/bin/xray'
-if os.path.isfile(file):
-  print(f'{file} exist.')
-  runService()
-  exit(0)
-else:
-  print(f'{file} not found.')
-
-create_dir('/tmp/xray/bin')
-
-url = 'https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip'
-savepath = '/tmp/xray/xray.zip'
-downloadFile(url, savepath)
-
-# unzip /tmp/xray/xray.zip -d /tmp/xray
-# install -m 755 /tmp/xray/xray /tmp/xray/bin/xray
-
-unzipFile(savepath, '/tmp/xray')
-print('---start install xray---')
-print(
-  subprocess.call(("install -m 755 /tmp/xray/xray /tmp/xray/bin/xray"),
-                  shell=True))
-
-shutil.copy('config.json', '/tmp/xray/bin/config.json')
-print('---copy config.json successs.---')
-runService()
