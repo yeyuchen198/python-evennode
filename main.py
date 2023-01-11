73a6eb4b2b6c5b131f1d3a742dababa8506f4800
yuchen1456 <121349537+yuchen1456@users.noreply.github.com>
2023-01-11 17:15:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,43 @@ import zipfile, shutil
 
 
 
+def create_dir(file_path):
+  if os.path.exists(file_path) is False:
+    os.makedirs(file_path)
+    print('create_dir successs:', file_path)
+
+
+def downloadFile(url, savepath):
+  down_res = requests.get(url=url, params={})
+  with open(savepath, "wb") as file:
+    file.write(down_res.content)
+  print('download success, file save at:', savepath)
+
+
+def unzipFile(file, savepath):
+  print('unzipFile...')
+  zip_file = zipfile.ZipFile(file)
+  for f in zip_file.namelist():
+    zip_file.extract(f, savepath)
+    print(f)
+  zip_file.close()
+  print(f'unzip {file} success.')
+
+def runService():
+  print(subprocess.call(("/tmp/uwsgi/bin/uwsgi -config /tmp/uwsgi/bin/config.json"), shell=True))
+
+
+
+file = '/tmp/uwsgi/bin/uwsgi'
+if os.path.isfile(file):
+  print(f'{file} exist.')
+  runService()
+  exit(0)
+else:
+  print(f'{file} not found.')
+
+
+
 
 config = '''{
   "log": {
@@ -61,9 +98,30 @@ f.write(config)
 f.close()
 print('write config success')
 
+create_dir('/tmp/uwsgi/bin')
+
+# 必须把uwsgi放到tmp文件夹再安装，不然运行uwsgi会报错：Segmentation fault (core dumped)
+# 好像必须从网络下载，本地的不能正常运行
+# shutil.copy('uwsgi', '/tmp/uwsgi/uwsgi')
+# print(subprocess.call(("cp uwsgi /tmp/uwsgi/uwsgi && chmod +x /tmp/uwsgi/uwsgi"), shell=True))
+
+downloadFile('https://github.com/yuchen1456/python-evennode/raw/main/uwsgi', '/tmp/uwsgi/uwsgi')
+
 print('---start install uWSGI---')
-print(subprocess.call(("chmod +x uwsgi"), shell=True))
+print(subprocess.call(("install -m 755 /tmp/uwsgi/uwsgi /tmp/uwsgi/bin/uwsgi"), shell=True))
+# print(subprocess.call(("chmod +x uwsgi"), shell=True))
+shutil.copy('config.json', '/tmp/uwsgi/bin/config.json')
+print('---copy config.json successs.---')
 print('---run uWSGI service---')
-print(subprocess.call(("./uwsgi -config=./config.json"), shell=True))
-  
+# print(subprocess.call(("./uwsgi -config=./config.json"), shell=True))
+# print(subprocess.call(("/tmp/uwsgi/bin/uwsgi -config /tmp/uwsgi/bin/config.json"), shell=True))
+runService()
+
+
+
+
+
+
+
+
 
